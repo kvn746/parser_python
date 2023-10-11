d11d58cd20fbbce1aa6777368f810f160e85b7ff
Konstantin Novikov <kvn746@gmail.com>
2023-10-11 15:45:41 +0300

    function page matches added

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import requests
 import pages.getUrls
 
 url = "https://keyfob.ru/brelki/brelki--pulti-distancionnogo-upravlenija-apollo/"
-links = pages.getUrls.getChildUrls(url)
-# html = requests.get(url)
-#
-# soup = bs(html.content, 'html5lib')
-#
-# links = []
-# for tag in soup.find_all('a'):
-#     link = str(tag.attrs.get('href'))
-#     if link.find('keyfob.ru') >= 0 and link.find('?') < 0 and link != 'https://keyfob.ru/':
-#         links.append(link)
-#
+page = pages.getUrls.getPage(url)
+links = pages.getUrls.getChildUrls(page)
+linksCondition = []
 for link in links:
-    print(link)
+    if link.find('keyfob.ru') >= 0 and link.find('?') < 0 and link != 'https://keyfob.ru/':
+        linksCondition.append(link)
+        if link.find('.html') >= 0:
+            print(link)
+            print(pages.getUrls.isPageMatched(pages.getUrls.getPage(link), 'product_info'))
+# for link in linksCondition:
+#     print(link)
 # print(soup.prettify())
 
--- pages/getUrls.py
+++ pages/getUrls.py
@@ -1,14 +1,23 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-def getChildUrls(url):
+def getPage(url):
     html = requests.get(url)
-    soup = bs(html.content, 'html5lib')
+
+    return bs(html.content, 'html5lib')
+
+def getChildUrls(page):
 
     links = []
-    for tag in soup.find_all('a'):
-        link = str(tag.attrs.get('href'))
-        if link.find('keyfob.ru') >= 0 and link.find('?') < 0 and link != 'https://keyfob.ru/':
-            links.append(link)
+    for tag in page.find_all('a'):
+        links.append(str(tag.attrs.get('href')))
 
     return links
+
+def isPageMatched(page, css_class):
+    result = 0
+    for tag in page.find_all('dev'):
+        if str(tag.attrs.get('class')) == css_class:
+            result = 1
+
+    return result
